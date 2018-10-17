f366cb742fb5863b071980b117981719c282f102
Krez90 <bilel.hamimed@yahoo.fr>
2018-10-17 11:57:29 +0200

    mabibi

---
+++ script.js
@@ -0,0 +1,9 @@
+var texte = document.getElementById("textequigrandit");
+
+function grossitLeTexte(){
+texte.style.fontSize ="30px";
+}
+
+document.addEventListener("click", grossitLeTexte);{
+console.log(texte);
+}
