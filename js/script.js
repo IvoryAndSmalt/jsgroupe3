1174282318c5f103d5b94d6f48fa17da8ef58ea2
IvoryAndSmalt <lucas180395@gmail.com>
2018-10-17 19:47:57 +0200

    c la viiiiiie

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var texte = document.getElementById("textequigrandit");
 texte.style.fontSize = "20px";
 
-texte.addEventListener("keyup", function () {
+texte.addEventListener("click", function () {
+
 if (texte.style.fontSize === "20px"){
 texte.style.fontSize = "30px";
 console.log(texte.style.fontSize);
 }
+
 else {
 texte.style.fontSize = "20px";
 console.log(texte.style.fontSize);
